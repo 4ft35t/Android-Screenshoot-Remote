93cd350428c816654065c7aa798cab08daef6fe5
git <git@127.1>
2021-08-09 10:17:03 +0800

    添加 30s 超时

--- cgi/cgi-bin/yunshanfu.sh
+++ cgi/cgi-bin/yunshanfu.sh
@@ -24,13 +24,16 @@ unlock(){
 screenshoot(){
      adb shell am start -n org.autojs.autojs/.external.open.RunIntentActivity -d /sdcard/Scripts/ysf.js
      sleep 3
+     start_time=$(date +%s)
      while true
      do
+         sleep 1
+         now=$(date +%s)
+         [ "$((now - start_time))" -gt 30 ] && break
          # 等文件写完再 pull
          modify_time=$(adb shell stat -c %Y $img)
-         now=$(date +%s)
-         [[ "$modify_time" && "$modify_time" -lt "$now" ]] && adb pull $img /tmp/ysf.png && break
-         sleep 1
+         [ "$modify_time" ] || continue
+         [ "$modify_time" -lt "$now" ] && adb pull $img /tmp/ysf.png && break
      done
 
      # press power button
